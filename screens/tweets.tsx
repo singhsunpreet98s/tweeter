253687c41a8661f0c9cf7c293d80fe64c9c414ef
singhsunpreet98s <singhsunpeet98s@gmail.com>
2021-05-01 15:01:09 +0530

    a sudden change

---
+++ screens/tweets.tsx
@@ -0,0 +1,14 @@
+export const tweets = [
+   {
+      name: 'Alina Grande',
+      userId: 'alienagrand',
+      img: 'https://www.arabnews.com/sites/default/files/2021/03/17/2526386-1520057592.jpg',
+      tweet: 'hey its cay from series victoria here is a new pic from show',
+      tweetImg: [
+         {
+            name: 'poster',
+            img: 'https://codelist.biz/wp-content/uploads/2020/05/1585387326_975828_1585387454_rrss_normal.jpg'
+         }
+      ]
+   }
+]
